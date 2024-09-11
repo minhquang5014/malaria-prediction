b81b1706f6cf8f6288beb358a3a48554d687699d
minhquang5014 <minhquang5014@gmail.com>
2024-09-11 09:39:35 +0700

    seq model with 1.8 million parameters

---
+++ sequential_model.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.layers import Conv2D, Activation, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Input, BatchNormalization
+
+def sequential_model(IMG_SIZE = 224):
+    model = Sequential()
+    model.add(Conv2D(16, kernel_size=3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=2, strides=2))
+
+    model.add(Conv2D(32, kernel_size=3, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=2, strides=2))
+
+    model.add(Flatten())
+    model.add(Dense(20, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+    return model
+
+model = sequential_model()
+model.summary()
