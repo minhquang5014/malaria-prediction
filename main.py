81f7c31f3296f331df069f749cc97abcd9b43244
minhquang5014 <minhquang5014@gmail.com>
2024-09-22 14:56:27 +0700

    trying to resolve the problem

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ dataset, dataset_info = sample_data()
 
 # split the data into train dataset, validation data, and test set
 train_dataset, val_dataset, test_dataset = splits(dataset[0], TRAIN_RATIO, VAL, TEST)
-image, label = train_dataset.take(1)
-print(image, label)
+
+print(train_dataset)
